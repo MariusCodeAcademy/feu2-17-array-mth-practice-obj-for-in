48101771bca18cc32ed1a665e4eea0aaf13210d9
Marius <marius@example.com>
2022-11-03 10:30:26 +0200

    people

---
+++ js/people.js
@@ -0,0 +1,69 @@
+let people = [
+  {
+    name: 'Jonas',
+    surname: 'Jonaitis',
+    sex: 'male',
+    age: 26,
+    income: 1200,
+    married: false,
+    hasCar: false,
+  },
+  {
+    name: 'Severija',
+    surname: 'Piktutytė',
+    sex: 'female',
+    age: 26,
+    income: 1300,
+    married: false,
+    hasCar: true,
+  },
+  {
+    name: 'Valdas',
+    surname: 'Vilktorinas',
+    sex: 'male',
+    age: 16,
+    income: 0,
+    married: false,
+    hasCar: false,
+  },
+  {
+    name: 'Virginijus',
+    surname: 'Uostauskas',
+    sex: 'male',
+    age: 32,
+    income: 2400,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: 'Samanta',
+    surname: 'Uostauskienė',
+    sex: 'female',
+    age: 28,
+    income: 1200,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: 'Janina',
+    surname: 'Stalautinskienė',
+    sex: 'female',
+    age: 72,
+    income: 364,
+    married: false,
+    hasCar: false,
+  },
+];
+
+// 1. atspausdinti visus vyrus
+
+// 2. konsoleje atspausdinti visas moteris jaunesnes nei 35 metai
+
+// 3. atrinkti i nauja masyva visus žmones kurie turi mašinas
+
+// 4. i nauja masyva atrinkti susituokusiu zmoniu vardus
+
+// 5. i nauja masyva atrinkti vairuojanciu zmoniu lytis.
+
+// 6. su funkcija grazinti objekta kuriame butu fairuojanciu vyru ir moteru kiekiai
+// pvz {vyrai: 4, moterys: 3}
